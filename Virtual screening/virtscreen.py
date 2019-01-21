bd6de99a8664fbd1da94a658a63820be58de7326
Somdeb Chatterjee <somdeb.ch@gmail.com>
2019-01-21 20:14:47 +0530

    MORE code cleanups.

--- Virtual screening/virtscreen.py
+++ Virtual screening/virtscreen.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
-
-
 """
 Automated virtual screening & analysis with Autodock Vina
 """
-
 """
 REQUIREMENTS:
 
@@ -44,8 +41,8 @@ class Screen:
             repeats) + "-repeats-" + "-" + str(dt.date.today()) + ".csv"
         self.resfile = resfile + "-" + str(repeats) + "-repeats-" + "-" + str(
             dt.date.today()) + ".csv"
-        self.ldir = ligand_directory
-        self.rdir = output_directory
+        self.ligand_dir = ligand_directory
+        self.result_dir = output_directory
         self.repdir = repeats_directory
         self.minimize = minimization
         self.vinaconf = config
@@ -56,6 +53,12 @@ class Screen:
         ]  # list of lists containing binding energy sampling data per ligand
         self.bestbinders = [
         ]  # holds the names of the top five best binding ligands
+        self.energies = [
+        ]  # list containing raw binding energy values per ligand for t-test
+
+        self.chunks = [
+        ]  # list containing mean and SD of binding energy / ligand
+        self.rawvars = []  # holds raw binding energies per ligand temporarily
 
         header_tmp = ["Ligand", "mean-dG", "stdev-dG", "#samples"]
         self.stats.append(header_tmp.copy())
@@ -77,20 +80,22 @@ class Screen:
             )
             raise SystemExit()
 
+
     def preprocess_ligands(self):
         print("Converting ligands, assuming structures are already in 3D\n\n")
-        for file in os.listdir(self.ldir):
-            if file.endswith(".sdf"):
-                print(
-                    "Converting ligand: " + os.path.splitext(file)[0] + "\n\n")
+        for filename in os.listdir(self.ligand_dir):
+            if filename.endswith(".sdf"):
+                print("Converting ligand: " + os.path.splitext(filename)[0] +
+                      "\n\n")
                 if self.minimize:
                     print(
                         "Minimizing ligands with steepest descent and GAFF force field ...\n\n"
                     )
                     sp.call(
                         [
-                            "obabel -i sdf " + self.ldir + file + " -O " +
-                            self.ldir + os.path.splitext(file)[0] + ".sdf" +
+                            "obabel -i sdf " + self.ligand_dir + filename +
+                            " -O " + self.ligand_dir +
+                            os.path.splitext(filename)[0] + ".sdf" +
                             " --minimize --ff GAFF --sd"
                         ],
                         shell=True)
@@ -100,9 +105,9 @@ class Screen:
 
                 sp.call(
                     [
-                        "obabel -i sdf " + self.ldir + file + " -O " +
-                        self.ldir + os.path.splitext(file)[0] + ".pdbqt" +
-                        " -p -partialcharge gasteiger"
+                        "obabel -i sdf " + self.ligand_dir + filename + " -O "
+                        + self.ligand_dir + os.path.splitext(filename)[0] +
+                        ".pdbqt" + " -p -partialcharge gasteiger"
                     ],
                     shell=True)
 
@@ -110,146 +115,103 @@ class Screen:
                 print("Could not find ligand SDFs, exiting ...\n\n")
                 raise SystemExit()
 
+
+    def dock_molecule(self, ligand_name):
+
+        pathname = self.result_dir + os.path.splitext(ligand_name)[0]
+        os.makedirs(pathname)
+
+        for rep in range(self.repeats):  # from 0 to self.repeats-1
+            os.makedirs(pathname + "/" + str(rep))
+            sp.call(
+                [
+                    "vina --config " + self.vinaconf + " --ligand " +
+                    self.ligand_dir + ligand_name + " --out " + pathname +
+                    "/" + str(rep) + "/" + "out" + "-pass-" + str(rep) + "-" +
+                    ligand_name + " --log " + pathname + "/" + str(rep) + "/" +
+                    "log_" + "pass_" + str(rep) + "_" +
+                    os.path.splitext(ligand_name)[0] + ".txt" +
+                    " --exhaustiveness " + str(self.rigor)
+                ],
+                shell=True)
+
+
     def start_virtual_screen(self, mode="SINGLE_PASS"):
 
         print("\nStarting virtual screening run ...\n\n")
 
-        for file in os.listdir(self.ldir):
+        for filename in os.listdir(self.ligand_dir):
 
             if mode == "REPLICATED" and str(
-                    os.path.splitext(file)
-                [0]) in self.bestbinders[0] and file.endswith(".pdbqt"):
+                    os.path.splitext(filename)
+                [0]) in self.bestbinders[0] and filename.endswith(".pdbqt"):
 
-                basename = self.ldir + os.path.splitext(file)[0]
-                pathname = self.rdir + os.path.splitext(file)[0]
-                os.makedirs(pathname)
-                for rep in range(self.repeats):  # from 0 to self.repeats-1
-                    os.makedirs(pathname + "/" + str(rep))
-                    sp.call(
-                        [
-                            "vina --config " + self.vinaconf + " --ligand " +
-                            self.ldir + file + " --out " + self.rdir +
-                            os.path.splitext(file)[0] + "/" + str(rep) + "/" +
-                            "out" + "-pass-" + str(rep) + "-" + file +
-                            " --log " + self.rdir + os.path.splitext(file)[0] +
-                            "/" + str(rep) + "/" + "log_" + "pass_" +
-                            str(rep) + "_" + os.path.splitext(file)[0] +
-                            ".txt" + " --exhaustiveness " + str(self.rigor)
-                        ],
-                        shell=True)
+                self.dock_molecule(filename)
 
-            elif mode == "SINGLE_PASS" and file.endswith(".pdbqt"):
+            elif mode == "SINGLE_PASS" and filename.endswith(".pdbqt"):
 
-                basename = self.ldir + os.path.splitext(file)[0]
-                pathname = self.rdir + os.path.splitext(file)[0]
-                os.makedirs(pathname)
-                for rep in range(self.repeats):  # from 0 to self.repeats-1
-                    os.makedirs(pathname + "/" + str(rep))
-                    sp.call(
-                        [
-                            "vina --config " + self.vinaconf + " --ligand " +
-                            self.ldir + file + " --out " + self.rdir +
-                            os.path.splitext(file)[0] + "/" + str(rep) + "/" +
-                            "out" + "-pass-" + str(rep) + "-" + file +
-                            " --log " + self.rdir + os.path.splitext(file)[0] +
-                            "/" + str(rep) + "/" + "log_" + "pass_" +
-                            str(rep) + "_" + os.path.splitext(file)[0] +
-                            ".txt" + " --exhaustiveness " + str(self.rigor)
-                        ],
-                        shell=True)
+                self.dock_molecule(filename)
 
             else:
                 continue
 
+
+    def extract_data_from_pdbqt(self, filename):
+
+        basename = os.path.splitext(filename)[0]
+        pathname = self.result_dir + os.path.splitext(filename)[0]
+        self.chunks.append(basename)
+        self.energies.append(basename)
+
+        for rep in range(self.repeats):  # from 0 to self.repeats-1
+            logname = "log_" + "pass_" + str(rep) + "_" + os.path.splitext(
+                filename)[0] + ".txt"
+            target = pathname + "/" + str(rep) + "/" + logname
+            # WARNING: the below command will fail if vina does not generate 9 models
+            # because it expects the file to have a certain number of lines, which can
+            # vary if the number of models changes. Consider a more robust implementation.
+            # Extract the binding energy from a Vina log file.
+            cmdline = "tail -n+26 " + target + " | awk '/-/ { print $2 }' | awk 'FNR==1'"
+
+            res = sp.Popen(
+                [cmdline], shell=True,
+                stdout=sp.PIPE).communicate()[0].strip().decode('ascii')
+            self.rawvars.append(float(res))
+            self.energies.append(float(res))
+
+        if self.repeats < 2:
+            # not enough data to calculate mean and stdev
+            # mean will be the single datapoint, stdev will be zero
+            self.chunks.append(self.rawvars[0])
+            self.chunks.append(0)
+            self.chunks.append(self.repeats)
+        else:
+            # we can calculate mean and stdev only if #repeats >= 2
+            self.chunks.append(stat.mean(self.rawvars))
+            self.chunks.append(stat.stdev(self.rawvars))
+            self.chunks.append(self.repeats)
+
+        del self.rawvars[:]
+        self.stats.append(self.chunks.copy())
+        del self.chunks[:]
+        self.sampling_stats.append(self.energies.copy())
+        del self.energies[:]
+
+
     def extract_binding_affinities(self, mode="SINGLE_PASS"):
-        print("\nProcessing results ...\n\n")
+        print("\nExracting affinities ...\n\n")
 
-        chunks = []  # list containing mean and SD of binding energy / ligand
-        rawvars = []  # holds raw binding energies per ligand temporarily
-        energies = [
-        ]  # list containing raw binding energy values per ligand for t-test
+        for filename in os.listdir(self.ligand_dir):
 
-        for file in os.listdir(
-                self.ldir):  # file is a builtin, consider changing the name
+            if filename.endswith(".pdbqt") and mode == "SINGLE_PASS":
 
-            if file.endswith(".pdbqt") and mode == "SINGLE_PASS":
-
-                basename = os.path.splitext(file)[0]
-                pathname = self.rdir + os.path.splitext(file)[0]
-                chunks.append(basename)
-                energies.append(basename)
-
-                for rep in range(self.repeats):  # from 0 to self.repeats-1
-                    logname = "log_" + "pass_" + str(
-                        rep) + "_" + os.path.splitext(file)[0] + ".txt"
-                    target = pathname + "/" + str(rep) + "/" + logname
-                    # WARNING the below command will fail if vina does not generate 9 models
-                    # because it expects the file to have a certain number of lines, which can
-                    # vary if the number of models changes. Consider a more robust implementation.
-                    cmdline = "tail -n+26 " + target + " | awk '/-/ { print $2 }' | awk 'FNR==2'"
-
-                    res = sp.Popen(
-                        [cmdline], shell=True, stdout=sp.PIPE).communicate()[
-                            0].strip().decode('ascii')
-                    rawvars.append(float(res))
-                    energies.append(float(res))
-
-                if self.repeats < 2:
-                    # not enough data to calculate mean and stdev
-                    # mean will be the single datapoint, stdev will be zero
-                    chunks.append(rawvars[0])
-                    chunks.append(0)
-                    chunks.append(self.repeats)
-                else:
-                    # we can calculate mean and stdev only if #repeats >= 2
-                    chunks.append(stat.mean(rawvars))
-                    chunks.append(stat.stdev(rawvars))
-                    chunks.append(self.repeats)
-
-                del rawvars[:]
-                self.stats.append(chunks.copy())
-                del chunks[:]
-                self.sampling_stats.append(energies.copy())
-                del energies[:]
+                self.extract_data_from_pdbqt(filename)
 
             elif mode == "REPLICATED" and str(
-                    os.path.splitext(file)
-                [0]) in self.bestbinders[0] and file.endswith(".pdbqt"):
+                    os.path.splitext(filename)
+                [0]) in self.bestbinders[0] and filename.endswith(".pdbqt"):
 
-                basename = os.path.splitext(file)[0]
-                pathname = self.rdir + os.path.splitext(file)[0]
-                chunks.append(basename)
-                energies.append(basename)
-
-                for rep in range(self.repeats):  # from 0 to self.repeats-1
-                    logname = "log_" + "pass_" + str(
-                        rep) + "_" + os.path.splitext(file)[0] + ".txt"
-                    target = pathname + "/" + str(rep) + "/" + logname
-                    cmdline = "tail -n+26 " + target + " | awk '/-/ { print $2 }' | awk 'FNR==2'"
-
-                    res = sp.Popen(
-                        [cmdline], shell=True, stdout=sp.PIPE).communicate()[
-                            0].strip().decode('ascii')
-                    rawvars.append(float(res))
-                    energies.append(float(res))
-
-                if self.repeats < 2:
-                    # not enough data to calculate mean and stdev
-                    # mean will be the single datapoint, stdev will be zero
-                    chunks.append(rawvars[0])
-                    chunks.append(0)
-                    chunks.append(self.repeats)
-                else:
-                    # we can calculate mean and stdev only if #repeats >= 2
-                    chunks.append(stat.mean(rawvars))
-                    chunks.append(stat.stdev(rawvars))
-                    chunks.append(self.repeats)
-
-                del rawvars[:]
-                self.stats.append(chunks.copy())
-                del chunks[:]
-                self.sampling_stats.append(energies.copy())
-                del energies[:]
+                self.extract_data_from_pdbqt(filename)
 
             else:
                 continue
@@ -264,22 +226,28 @@ class Screen:
         elif mode == "WITH_STATS":
             outfile = self.resfile
             datasrc = self.stats
+        else:
+            print(
+                "\nInvalid mode: "+mode+
+                " in print_results()\n\n"
+                )
+            raise SystemExit()
 
-        file = open(outfile, "w")
+        filename = open(outfile, "w")
         i = 0
         j = len(datasrc[0])
         for chunk in datasrc:
             for element in chunk:
                 i += 1
-                file.write(str(element))
+                filename.write(str(element))
                 if i != j:
-                    file.write(", ")
+                    filename.write(", ")
                 else:
                     continue
             i = 0
-            file.write("\n")
+            filename.write("\n")
 
-        file.close()
+        filename.close()
 
 
     def determine_bestbinders(self, num=5):
@@ -297,21 +265,22 @@ class Screen:
 
         self.bestbinders.append(ligand[:num])  # names of top 5 best binders
 
-    def verify_bestbinders(self,
-                          # outdir=self.repdir,
-                           repeats=5,
-                           exhaustiveness=20,
-                           resfile="replication_affinities_with_stats",
-                           sampfile="replication_affinities_only"):
+
+    def verify_bestbinders(
+            self,
+            repeats=5,
+            exhaustiveness=20,
+            resfile="replication_affinities_with_stats",
+            sampfile="replication_affinities_only"):
 
         print("\nVerifying best binders ...\n\n")
 
-        self.rdir = self.repdir
+        self.result_dir = self.repdir
         self.repeats = repeats
         self.rigor = exhaustiveness
 
-        self.sampfile = sampfile + "_" + str(
-            repeats) + "_repeats_" + str(dt.date.today()) + ".csv"
+        self.sampfile = sampfile + "_" + str(repeats) + "_repeats_" + str(
+            dt.date.today()) + ".csv"
         self.resfile = resfile + "_" + str(repeats) + "_repeats_" + str(
             dt.date.today()) + ".csv"
 
@@ -329,16 +298,17 @@ class Screen:
 
 
     def split_and_convert_pdbqts(self, mode="SINGLE_PASS"):
-        print("Splitting result files and (PDBQT -->  SDF) ... \n\n")
+        print("\nSplitting result files and (PDBQT -->  SDF) ... \n\n")
 
         target_dir = ""
 
         if mode == "SINGLE_PASS":
-            target_dir = self.rdir
+            target_dir = self.result_dir
         elif mode == "REPLICATED":
             target_dir = self.repdir
         else:
-            target_dir = self.rdir
+            print("\nInvalid mode: "+mode+" in split_and_convert_pdbqts()\n\n")
+            raise SystemExit()
 
         for dirs in os.listdir(target_dir):
             for dirs_1 in os.listdir(target_dir + dirs):
@@ -354,10 +324,10 @@ class Screen:
                             ["rm " + target_path + files],
                             shell=True)  # delete the concatenated file
                         # call openbabel for conversion
-                        for files in os.listdir(target_dir + dirs +
-                                                "/" + dirs_1 + "/"):
+                        for files in os.listdir(
+                                target_dir + dirs + "/" + dirs_1 + "/"):
                             if files.endswith(".pdbqt"):
-                                # extract binding info from PDBQT
+                                # extract binding energy, RMS min and max from PDBQT
                                 dG = sp.Popen(
                                     [
                                         "cat " + target_path + files +
@@ -406,19 +376,17 @@ def main():
         minimization=False,
         exhaustiveness=8)
 
-
     s.preprocess_ligands()
     s.start_virtual_screen()
     s.extract_binding_affinities()
-    s.determine_bestbinders(num=1)
+    s.determine_bestbinders(num=3)
 
     s.print_results(mode="SIMPLE")
     s.print_results(mode="WITH_STATS")
     s.split_and_convert_pdbqts(mode="SINGLE_PASS")
 
-    s.verify_bestbinders(repeats=3, exhaustiveness=10)
+    s.verify_bestbinders(repeats=3, exhaustiveness=8)
     s.split_and_convert_pdbqts(mode="REPLICATED")
-
 
     print("\n\n\nAll done!\n\n")
     raise SystemExit()
